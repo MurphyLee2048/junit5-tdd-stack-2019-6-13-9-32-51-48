7dd7e60b02febf643bf7fb29508b126238c6b290
murphylee2048 <1134697131@qq.com>
2019-07-12 16:08:18 +0800

    can return FizzBuzz when input is 15

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -2,5 +2,12 @@ package com.thoughtworks.tdd;
 
 public class FizzBuzz {
     public String convert(int number) {
+        String result = null;
+        switch (number) {
+            case 15:
+                result = "FizzBuzz";
+                break;
+        }
+        return result;
     }
 }
