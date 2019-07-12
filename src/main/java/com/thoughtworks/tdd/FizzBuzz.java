4bbb76c92a684e3b62599e591d8037c1a91c9c51
murphylee2048 <1134697131@qq.com>
2019-07-12 16:10:47 +0800

    fix error: should not use switch can return fizzbuzz when input is 15 can return buzz when input is 5

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -3,10 +3,11 @@ package com.thoughtworks.tdd;
 public class FizzBuzz {
     public String convert(int number) {
         String result = null;
-        switch (number) {
-            case 15:
-                result = "FizzBuzz";
-                break;
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if (number % 5 == 0) {
+            return "Buzz";
         }
         return result;
     }
