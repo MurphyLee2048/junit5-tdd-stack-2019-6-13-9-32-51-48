ab8b4a3fcb53ee305d45e5b5fc477507513495ba
murphylee2048 <1134697131@qq.com>
2019-07-12 16:04:05 +0800

    commit: delete useless import

--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -2,7 +2,6 @@ package com.thoughtworks.tdd;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
