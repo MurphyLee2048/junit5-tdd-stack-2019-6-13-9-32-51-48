102ed64bde0dee6498e54d19dbb357f0d794c81c
murphylee2048 <1134697131@qq.com>
2019-07-12 15:59:51 +0800

    commit test case.: should_return_1_when_convert_given_1/should_return_1_when_convert_given_2/should_return_1_when_convert_given_3

---
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -0,0 +1,54 @@
+package com.thoughtworks.tdd;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    @BeforeAll
+    static void initTestEnv() {
+    }
+
+    @BeforeEach
+    void initEveryMethod() {
+    }
+
+    @Test
+    public void should_return_1_when_convert_given_1() {
+        // given
+        int number = 1;
+
+        // when
+        String output = new FizzBuzz().convert(number);
+
+        // then
+        assertEquals(String.valueOf(number), output);
+    }
+
+    @Test
+    public void should_return_Fizz_when_convert_given_3() {
+        // given
+        int number = 3;
+
+        // when
+        String output = new FizzBuzz().convert(number);
+
+        // then
+        assertEquals("Fizz", output);
+    }
+
+    @Test
+    public void should_return_Buzz_when_convert_given_10() {
+        // given
+        int number = 10;
+
+        // when
+        String output = new FizzBuzz().convert(number);
+
+        // then
+        assertEquals("Buzz", output);
+    }
+}
